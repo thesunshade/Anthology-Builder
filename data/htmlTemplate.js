dced5a3960a565b73edbbca12a2b4fce62b975a7
thesunshade <82448383+thesunshade@users.noreply.github.com>
2024-07-25 09:03:22 +1200

    updates

---
+++ data/htmlTemplate.js
@@ -0,0 +1,19 @@
+export const HTML_OPEN = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${selection}</title>
+    <style>
+     /* Not all of this CSS may be necessary based on the build options you picked*/
+
+
+    */
+   </style>
+</head>
+<body>
+`;
+export const HTML_CLOSE = `
+</body>
+</html>`;
